6b425c86a3434ce9ac49df844bc691d54b1ba9ed
neksioo <100017725+neksioo@users.noreply.github.com>
2022-04-19 16:12:36 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,8 +1,29 @@
+@echo off
+MODE 80,1
+title [Flame Installer] - Installing modules: discum
 pip install discum
+cls
+title [Flame Installer] - Installing modules: ctypes
 pip install ctypes
+cls
+title [Flame Installer] - Installing modules: json
 pip install json
+cls
+title [Flame Installer] - Installing modules: string
 pip install string
+cls
+title [Flame Installer] - Installing modules: random
 pip install random
+cls
+title [Flame Installer] - Installing modules: os
 pip install os
+cls
+title [Flame Installer] - Installing modules: datetime
 pip install datetime
+cls
+title [Flame Installer] - Installing modules: colorama
 pip install colorama
+cls
+title [Flame Installer] - Modules installed
+timeout 2 >nul
+
